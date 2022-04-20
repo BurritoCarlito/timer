b5cd4eb7749e69cdcfbe538b9238e63b83574454
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-20 21:09:59 +0000

    updated, should return a beep when given a number representing time in the command line

--- timer1.js
+++ timer1.js
@@ -1,10 +1,8 @@
 let alarm = process.argv.slice(2);
-let delay = 0;
 
 for (const secs of alarm) {
   setTimeout(() => {
     if (!isNaN(secs) && secs >= 1) {
     } process.stdout.write('\x07');
-  }, delay);
-  delay += (secs * 1000);
+  }, (secs * 1000));
 };
